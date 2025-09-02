7e8e72c365661d4af19e88ea9b8200938f49bccf
KevinMarin-collab <kevin.marin@campusucc.edu.co>
2025-09-02 14:10:34 +0000

    Estructuras for y while

---
+++ EstructurasRepe.java
@@ -0,0 +1,13 @@
+public class EstructurasRepe {
+    public static void main(String[] args) {
+        for (int i = 0; i > 5; i ++) { 
+            System.out.println(i)
+        }
+
+        int j = 1;
+        while (j<=5){
+            System.out.println("Me gustan las mujeres")
+            j++
+        }
+    }
+}
